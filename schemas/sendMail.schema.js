dda721d0b17e82a5eeb966268b979aaeb899549f
LeonAngelit <leo63582@gmail.com>
2022-07-08 23:28:28 +0200

    regex

--- schemas/sendMail.schema.js
+++ schemas/sendMail.schema.js
@@ -1,9 +1,9 @@
 const Joi = require('joi');
 
 
-const name = Joi.string().min(3).max(30).pattern(/^[a-zA-Z\s\.]*$/);
+const name = Joi.string().min(3).max(60).pattern(/^([a-zA-ZÀ-ÿ\u00f1\u00d1]\s*\.*){2,60}$/);
 const mailSender = Joi.string().email();
-const phone = Joi.string().length(9).pattern(/^[0-9]+$/);
+const phone = Joi.string().length(9).pattern(/^[6798]\d{8}$/);
 const subject = Joi.string().min(4).max(140);
 const htmlBody = Joi.string().min(5);
 
